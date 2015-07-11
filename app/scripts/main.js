cb894e61cead09daa7db640ed84484cc9690bb80
Jose Medina <josemedina921@gmail.com>
2015-07-11 17:38:20 -0400

    Swapping the order to point to the CDN first

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
 require.config({
   paths: {
-    'jquery': ['../bower_components/jquery/dist/jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min'],
+    'jquery': ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min', '../bower_components/jquery/dist/jquery'],
     'backbone': ['../bower_components/backbone/backbone'],
     'underscore': ['../bower_components/underscore/underscore-min'],
     'text': ['../bower_components/text/text'],
     'mustache': ['../bower_components/mustache/mustache'],
-    'bootstrap': ['../bower_components/dist/js/bootstrap.min.js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'],
+    'bootstrap': ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', '../bower_components/dist/js/bootstrap.min.js'],
     'Templates': 'templates/Templates'
   }
 });
