0989f8eab78139fc302e2739b8ba763e76c74f60
Jose Medina <josemedina921@gmail.com>
2015-07-27 22:41:47 -0400

    update the dependencies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,13 @@ require.config({
   enforceDefine: true,
   paths: {
     'jquery': ['https://code.jquery.com/jquery-2.1.4.min', '../bower_components/jquery/dist/jquery'],
-    'backbone': ['../bower_components/backbone/backbone'],
-    'underscore': ['../bower_components/underscore/underscore-min'],
-    'text': ['../bower_components/text/text'],
-    'mustache': ['../bower_components/mustache/mustache'],
+    'backbone': ['https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.1/backbone-min.js', '../bower_components/backbone/backbone'],
+    'underscore': ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js', '../bower_components/underscore/underscore-min'],
+    'text': ['https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min.js', '../bower_components/text/text'],
+    'mustache': ['https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.1.3/mustache.min.js', '../bower_components/mustache/mustache'],
     'bootstrap': ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', '../bower_components/dist/js/bootstrap.min.js'],
     'Templates': 'templates/Templates',
-    'marionette': ['../bower_components/marionette/lib/backbone.marionette.min.js']
+    'marionette': ['https://cdnjs.cloudflare.com/ajax/libs/backbone.marionette/2.4.2/backbone.marionette.min.js', '../bower_components/marionette/lib/backbone.marionette.min.js']
   }
 });
 
