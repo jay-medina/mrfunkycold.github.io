85ed32e1c23788fc128b5f9dac94c0db5421c431
Jose Medina <josemedina921@gmail.com>
2015-07-27 22:14:44 -0400

    Revamping the homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,48 +7,14 @@ require.config({
     'text': ['../bower_components/text/text'],
     'mustache': ['../bower_components/mustache/mustache'],
     'bootstrap': ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', '../bower_components/dist/js/bootstrap.min.js'],
-    'Templates': 'templates/Templates'
+    'Templates': 'templates/Templates',
+    'marionette': ['../bower_components/marionette/lib/backbone.marionette.min.js']
   }
 });
 
 define(function(require) {
   'use strict';
 
-  var $ = require('jquery');
-  var $main = $('.homepage_main');
-  var DURATION = 400;
 
-  var background_images = [
-    'app/images/astoria.jpg',
-    'app/images/mets.jpg',
-    'app/images/queens.jpg',
-    'app/images/working.jpg'
-  ];
-
-  var image_count = 0;
-
-  function rotateImagesAfterTenSeconds() {
-    setTimeout(rotateImages, 10000);
-  }
-
-  function createUrlString(url) {
-    return 'url(' + url + ')';
-  }
-
-  function rotateImages() {
-
-    $main.fadeOut(DURATION, function() {
-      $main.css('background-image', createUrlString(background_images[image_count]));
-      image_count += 1;
-
-      if (image_count === background_images.length) {
-        image_count = 0;
-      }
-      $main.fadeIn(DURATION, rotateImagesAfterTenSeconds);
-    });
-  }
-
-
-  rotateImagesAfterTenSeconds();
 
 });
