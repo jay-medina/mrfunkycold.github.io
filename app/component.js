99b7cea4772cc33c72555397b00c4d83f49dbc8e
Jose Medina <josemedina921@gmail.com>
2016-08-05 13:10:47 -0700

    separating out dev server config from common config

--- app/component.js
+++ app/component.js
@@ -3,5 +3,11 @@ module.exports = function() {
 
   element.innerHTML = 'hello world';
 
+  var div = document.createElement('div');
+
+  div.innerHTML = "now will it work";
+
+  element.appendChild(div);
+
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,13 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 }
 
+//merges properties together
 const merge = require('webpack-merge');
+
+// checks to ensure that config is correct
+const validate = require('webpack-validator'); 
+
+const parts = require('./libs/parts');
 
 const commonConfig = {
   // Entry accepts a path or an object of entries.
@@ -30,9 +36,15 @@ var config;
 
 switch(process.env.npm_lifecycle_event) {
   case 'build':
-    config = merge(common, {}); break;
+    config = merge(commonConfig, {}); break;
   default:
-    config = merge(common, {});
+    config = merge(
+      commonConfig, 
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
-module.exports = config;
+module.exports = validate(config);
