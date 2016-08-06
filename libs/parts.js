5c6953cf9f5ae4bb8f9f80d8b6fedc61f4cebe6b
Jose Medina <josemedina921@gmail.com>
2016-08-05 22:55:01 -0700

    adding define plugin which sets global variables

--- libs/parts.js
+++ libs/parts.js
@@ -46,3 +46,15 @@ exports.minify = function() {
     ]
   };
 }
+
+
+exports.setFreeVariable = function(key, value) {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ switch(process.env.npm_lifecycle_event) {
   case 'build':
     config = merge(
       commonConfig,
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
       parts.minify(),
       parts.setupCSS(PATHS.app)); 
     
