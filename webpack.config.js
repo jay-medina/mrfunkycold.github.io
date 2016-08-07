0023bca775c86bec262438bb440d7e1611f52a32
Jose Medina <josemedina921@gmail.com>
2016-08-06 20:26:13 -0700

    Cleaning up the build directory

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ switch(process.env.npm_lifecycle_event) {
           filename: '[name].[chunkhash].js'
         }
       },
+      parts.clean(PATHS.build),
       parts.setFreeVariable(
         'process.env.NODE_ENV',
         'production'
